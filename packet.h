83a00ec8a8ca4a8c75d936d0692ae5ab01d4b933
seo <hey_minj@naver.com>
2025-08-08 04:01:14 -0400

    add Info struct to test arp-spoof

--- packet.h
+++ packet.h
@@ -2,6 +2,8 @@
 #pragma pack(1)
 #include "ethernet.h"
 #include "arp.h"
+#include "ip.h"
+#include "mac.h"
 
 struct ArpPacket{
 private:
@@ -20,3 +22,22 @@ public:
         return arp;
     }
 };
+
+struct Info{
+    Mac s_mac_;
+    Ip s_ip_;
+    Mac d_mac_;
+    Ip d_ip_;
+    Mac my_mac_;
+    Ip my_ip_;
+
+    Info(char* dev, Mac s_mac, Ip s_ip, Mac d_mac, Ip d_ip){
+        s_mac_ = s_mac;
+        s_ip_ = s_ip;
+        d_mac_ = d_mac;
+        d_ip_ = d_ip;
+        my_mac_ = get_my_mac(dev);
+        my_ip_ = get_my_ip(dev);
+    }
+}
+
